31c13656fba398bd2c7cfcdd8c26c4e406725043
= <=>
2025-02-04 20:23:46 +0100

    canvas un poco avanzado pero no mucho mas de lo que habia

--- Interfaces/AE4/AE4.3/scripts.js
+++ Interfaces/AE4/AE4.3/scripts.js
@@ -1,5 +1,36 @@
 //Constants
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("canvas"); 
+const ctx = canvas.getContext("2d"); 
+const width = canvas.width; 
+const height = canvas.height; 
+const size = 10;
+const speed = 100;
+
+//Colors
+const snakeColor = "green";
+const appleColor = "red";
+const trapColor = "blue";
+const scoreColor = "white";
+
+//Direction
+const UP = 38;
+const DOWN = 40;
+const LEFT = 37;
+const RIGHT = 39;
+
+//Initial position
+let x = width/2;
+let y = height/2;
+
+//Movement
+let dx = size;
+let dy = 0;         
+
+//Variables
+let score = 0;
+let snake = [];
+let apple = {x: 0, y: 0};
+
 
 
 //Classes
@@ -12,7 +43,7 @@ const canvas = document.getElementById("canvas");
 * @param color: string
 *
 */
-class segmento{
+class Segmento{
     constructor(x, y, size, color){
         this.x = x;
         this.y = y;
@@ -31,7 +62,17 @@ class segmento{
      
 }
 
-let pj = new segmento(0, 0, 10, "green");
+class Juego{
+    constructor(){};
+    iniciarJuego(){};
+    cambiarDireccion(e){};
+    actualizar(){};
+    moverSnake(){};
+    verificarColisiones(){};
+    generarCoordenadaAleatoria(max){};
+    generarTrampas(n){}
+    reiniciarJuego(){};
+}
 
-pj.draw(canvas.getContext("2d"));
-
+let pj = new Segmento(x, y, size, snakeColor);
+pj.draw(ctx);
